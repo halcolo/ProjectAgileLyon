f8129976f8d9d372bd4f9cf8ea4072a7a2c854d8
halcolo <juandiego.alfonsoocampo@gmail.com>
2023-12-03 15:34:01 +0100

    Implementing docstrings

--- api_routes/user.py
+++ api_routes/user.py
@@ -1,20 +1,34 @@
 from flask import jsonify
 from flask import request
 from flask.views import MethodView
-from packages.planning_poker import PlanningPoker
 from utils.utils import response
 
 
 class PlayerView(MethodView):
-    # Example usage:
+    """
+    A class representing the API endpoints for managing players.
+    """
+
     def __init__(self) -> None:
         super().__init__()
         
     def get(self):
-        
+        """
+        Get method for retrieving player information.
+
+        Returns:
+            A JSON response with a message.
+        """
         return jsonify({"message": "Hello, world!"})
     
     def post(self):
+        """
+        Post method for adding a new player.
+
+        Returns:
+            A response indicating that the player has been added.
+        """
         name = request.args.get('name')
         self.game.add_player(name)
+        # Player(name).create_user()
         return response("Player added")
